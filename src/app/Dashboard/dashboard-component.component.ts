74f1419283072eef65160ba9a2672a7a9d2affbd
Sai Raj <sai244252@gmail.com>
2020-05-29 08:37:36 +0530

    First commit

--- src/app/Dashboard/dashboard-component.component.ts
+++ src/app/Dashboard/dashboard-component.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
     selector : 'app-dashBoard',
     templateUrl : './dashboard-component.component.html',
-    styleUrls : []
+    styleUrls : ['./dashboard-component.component.css']
 })
 export class DashBoardComponent{
 
